3b762693cc2744cbff323aa5ebb082fec2485dbd
chris.j.carter <chris.j.carter91@gmail.com>
2020-01-01 19:36:24 -0700

    Numbers and operators implemented

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = ({number, incrementCount}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className={`num${incrementCount} number`}>{number}</button>
     </>
   );
 };
+
+export default NumberButton;
